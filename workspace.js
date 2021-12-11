87b3b3c9f22a5491dbada7861379a1c6cd19224b
Philip Villasurda <81870118+philrae2@users.noreply.github.com>
2021-12-11 16:07:44 +0000

    Added BAsic Exercises folder

--- workspace.js
+++ workspace.js
@@ -1,11 +1,1 @@
-let rlSync = require("readline-sync");
-let response = "yes";
-
-function prompt () {
-  return rlSync.question ("Do you want to continue? Enter yes or no\n");
-}
-
-
-do {
-  response = prompt();
-} while (response === "yes");
+console.log(/o/.test("bobcat"));
