90fe7d8afe56959d11ea42a5b826f3cadef993f0
Philip Villasurda <81870118+philrae2@users.noreply.github.com>
2021-12-20 05:41:30 +0000

    JS 100 Exercises

--- workspace.js
+++ workspace.js
@@ -1,1 +1,9 @@
-console.log(/o/.test("bobcat"));
+
+for (let row=1; row <=5; row++) {
+  let string = "";
+  
+  for ( let counter = 1; counter <= 6 - row ; counter++) {
+    string += "#";
+  }
+  console.log(string);
+}
