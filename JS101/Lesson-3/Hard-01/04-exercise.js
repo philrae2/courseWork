eb41417ece89c2d58e0f22a92d74687ac9ad95ff
Philip Villasurda <81870118+philrae2@users.noreply.github.com>
2022-01-09 20:54:52 +0000

    "updates"

---
+++ JS101/Lesson-3/Hard-01/04-exercise.js
@@ -0,0 +1,30 @@
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
+
+  return false;
+}
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  
+  if (dotSeparatedWords.length === 4) {
+    while (dotSeparatedWords.length > 0) {
+      let word = dotSeparatedWords.pop();
+      if (!isAnIpNumber(word)) {
+        return false;
+        break;
+      }
+    }
+      return true;
+    } else {
+      return false;
+  }
+}
+
+
+console.log(isDotSeparatedIpAddress("1.2.3.4.5"));
+console.log(isDotSeparatedIpAddress("1.2.3.4"))
+console.log(isDotSeparatedIpAddress("1.2.3"))
